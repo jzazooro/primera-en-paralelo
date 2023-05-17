ee6bf43c8c3a6d5187db96ae5ba13c0851513cfb
jzazooro <91785177+jzazooro@users.noreply.github.com>
2023-05-17 07:53:24 +0200

    scrapeo

---
+++ scrapeo.py
@@ -0,0 +1,9 @@
+import random
+from time import sleep
+
+def scrape(url):
+    print("starting", url)
+    duration = round(random.random(),3)
+    sleep(duration)
+    print("finished", url, "time taken:", duration, "seconds")
+    return url, duration
